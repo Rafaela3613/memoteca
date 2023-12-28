f5147c788877bed6a2dd0d5b37a99ec0cc9924c3
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2023-12-27 23:09:47 -0300

    incluindo o ngIf

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -9,15 +9,15 @@ export class ListarPensamentoComponent implements OnInit {
 
   listaPensamentos = [
     {
-    conteudo: 'Passo informaçoes para o componete filho',
-    autoria: 'Componente pai',
-    modelo: 'modelo3'
-  },
-  {
-    conteudo: 'Minha propriedade é decorada com @input()',
-    autoria: 'Componente filho',
-    modelo: 'modelo2'
-  }
+      conteudo: 'Passo informaçoes para o componete filho',
+      autoria: 'Componente pai',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @input()',
+      autoria: 'Componente filho',
+      modelo: 'modelo2'
+    }
 ];
 
   constructor() { }
